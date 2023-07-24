ba46a8f3358c4dbdec5ad6f428944bcaebcf2ee5
Sravan T P <sravantp03@gmail.com>
2023-07-24 15:39:38 +0530

    added tests for name and createRequest functions

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 const POLYGON_RPC_URL = process.env.POLYGON_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API = process.env.ETHERSCAN_API;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -13,6 +14,9 @@ module.exports = {
       accounts: [PRIVATE_KEY],
       chainId: 80001,
     },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API,
   },
   solidity: "0.8.19",
 };
--- test/PaypalTest.js
+++ test/PaypalTest.js
@@ -15,4 +15,45 @@ describe("Paypal", function () {
       expect(owner).to.equal(await signer.getAddress());
     });
   });
+
+  describe("addName", function () {
+    it("user should able to add name to address", async function () {
+      const [, user] = await ethers.getSigners();
+      const tx = await Paypal.connect(user).addName("USER");
+      await tx.wait();
+
+      const [expectedResult] = await Paypal.getMyName(await user.getAddress()); // return the data as array of 2 items name and hasName
+      expect(expectedResult).to.equal("USER");
+    });
+  });
+
+  describe("createRequest", function () {
+    it("user should able to create a new request", async function () {
+      // adding name to user1
+      const [, user1, user2] = await ethers.getSigners();
+      const tx1 = await Paypal.connect(user1).addName("SAM");
+      await tx1.wait();
+
+      // ADDING NAME TO user2
+      const tx = await Paypal.connect(user2).addName("BEN");
+      await tx.wait();
+
+      // creating a new request to user2 by user1
+      const tx2 = await Paypal.connect(user1).createRequest(
+        await user2.getAddress(),
+        ethers.parseEther("1"),
+        "Movie Snacks"
+      );
+      await tx2.wait();
+
+      const data = await Paypal.connect(user2).getMyRequests(
+        await user2.getAddress()
+      ); // RETURN AN ARRAY OF ARRAYS OF VALUES
+
+      expect(data[0][0]).to.equal(await user1.getAddress());
+      expect(data[1][0]).to.equal(ethers.parseEther("1"));
+      expect(data[2][0]).to.equal("Movie Snacks");
+      expect(data[3][0]).to.equal("SAM");
+    });
+  });
 });
