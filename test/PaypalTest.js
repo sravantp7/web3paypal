12212d1f8e651a912c480b9d1bdf2182e1f4068c
Sravan T P <sravantp03@gmail.com>
2023-07-25 14:31:22 +0530

    added test to test payRequest function

--- test/PaypalTest.js
+++ test/PaypalTest.js
@@ -4,7 +4,7 @@ const { assert, expect } = require("chai");
 describe("Paypal", function () {
   let Paypal;
   beforeEach(async function () {
-    Paypal = await ethers.deployContract("Paypal");
+    Paypal = await ethers.deployContract("Paypal"); // here first account will be used for deployment
     await Paypal.waitForDeployment();
   });
 
@@ -54,6 +54,52 @@ describe("Paypal", function () {
       expect(data[1][0]).to.equal(ethers.parseEther("1"));
       expect(data[2][0]).to.equal("Movie Snacks");
       expect(data[3][0]).to.equal("SAM");
+    });
+  });
+
+  describe("payRequest", function () {
+    it("should able to pay the request", async function () {
+      // create a request.
+      const [, user1, user2] = await ethers.getSigners();
+      // adding name
+      const tx1 = await Paypal.connect(user1).addName("JOY");
+      await tx1.wait();
+
+      const tx2 = await Paypal.connect(user2).addName("JIM");
+      await tx2.wait();
+
+      // USER1 Creating request to user2
+      const tx3 = await Paypal.connect(user1).createRequest(
+        await user2.getAddress(),
+        ethers.parseEther("10"),
+        "Pocket Money"
+      );
+      await tx3.wait();
+
+      const user1BalanceAfterRequest = await ethers.provider.getBalance(
+        await user1.getAddress()
+      );
+
+      const data = await Paypal.connect(user2).getMyRequests(
+        await user2.getAddress()
+      );
+      expect(data[0][0]).to.equal(await user1.getAddress());
+
+      // user2 paying the request at index 0
+      const tx4 = await Paypal.connect(user2).payRequest(0, {
+        value: ethers.parseEther("10"),
+      });
+      await tx4.wait();
+
+      // getting the new eth balance of user1
+      const user1NewBalance = await ethers.provider.getBalance(
+        await user1.getAddress()
+      );
+
+      // checking whether the new balance of user1 is equal to old balance + requested amount
+      expect(user1NewBalance).to.equal(
+        user1BalanceAfterRequest + ethers.parseEther("10")
+      );
     });
   });
 });
